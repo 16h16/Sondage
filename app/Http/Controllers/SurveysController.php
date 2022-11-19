f519e2fd123209fbdc5f49de765d9f3a20bc8eeb
Hamza Echamlali <echamlali.hamza@gmail.com>
2022-11-19 11:45:32 +0100

    Added Surveys model/controller/migration
    Added Surveys model/controller/migration

---
+++ app/Http/Controllers/SurveysController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Surveys;
+use Illuminate\Http\Request;
+
+class SurveysController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Surveys  $surveys
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Surveys $surveys)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Surveys  $surveys
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Surveys $surveys)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Surveys  $surveys
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Surveys $surveys)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Surveys  $surveys
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Surveys $surveys)
+    {
+        //
+    }
+}
